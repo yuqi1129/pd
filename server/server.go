9b1baad56c2c5c4bd947c179f625b59ffa3826a9
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2020-12-01 14:50:54 +0800

    Persist temporary setting to etcd  (#3131) (#3228)
    * cherry pick #3131 to release-4.0
    
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>
    
    * fix rebasing error
    
    Signed-off-by: longfangsong <longfangsong@icloud.com>
    
    Co-authored-by: 龙方淞 <longfangsong@icloud.com>

--- server/server.go
+++ server/server.go
@@ -454,7 +454,6 @@ func (s *Server) Run() error {
 	if err := s.startEtcd(s.ctx); err != nil {
 		return err
 	}
-
 	if err := s.startServer(s.ctx); err != nil {
 		return err
 	}
@@ -1149,7 +1148,10 @@ func (s *Server) campaignLeader() {
 		return
 	}
 	defer s.stopRaftCluster()
-
+	if err := s.persistOptions.LoadTTLFromEtcd(s.ctx, s.client); err != nil {
+		log.Error("failed to load persistOptions from etcd", errs.ZapError(err))
+		return
+	}
 	s.member.EnableLeader()
 	defer s.member.DisableLeader()
 
@@ -1253,8 +1255,11 @@ func (s *Server) PersistFile(name string, data []byte) error {
 }
 
 // SaveTTLConfig save ttl config
-func (s *Server) SaveTTLConfig(data map[string]interface{}, ttl time.Duration) {
+func (s *Server) SaveTTLConfig(data map[string]interface{}, ttl time.Duration) error {
 	for k, v := range data {
-		s.persistOptions.SetTTLData(s.ctx, k, v, ttl)
+		if err := s.persistOptions.SetTTLData(s.ctx, s.client, k, fmt.Sprint(v), ttl); err != nil {
+			return err
+		}
 	}
+	return nil
 }
